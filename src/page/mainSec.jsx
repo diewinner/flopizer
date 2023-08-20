f4ff7a103ca6174a008774543e4e1ba6b0da9930
Ilnur <112321302+diewinner@users.noreply.github.com>
2023-08-20 18:54:22 +0300

    end mainSec

--- src/page/mainSec.jsx
+++ src/page/mainSec.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Cube from '../assets/img/Cube.jpg'
 import firstChild from "../assets/img/firstChild.jpg";
 import secChild from "../assets/img/secChild.jpg";
+import screenShot from '../assets/img/screenShotPostFlopizer.jpg'
 export const MainSec = ({ cls }) => {
     return (
         <div className={cls.main_container}>
@@ -61,6 +62,33 @@ export const MainSec = ({ cls }) => {
                 </div>
             </div>
 
+            <div className={cls.strategy_container}>
+                <div className={cls.strategy_text}>
+                    <div className={cls.strategy_text__title}>
+                        Strategy Editor
+                    </div>
+                    <div className={cls.strategy_text__subtitle}>
+                        Lock any hand, customize any nod
+                    </div>
+                    <div className={cls.strategy_text__btn}>
+                        Learn more >
+                    </div>
+                    <ul className={cls.strategy_text__list}>
+                        <li className={cls.strategy_text__list__item}>Quick Start Mode</li>
+                        <li className={cls.strategy_text__list__item}>5 Analytical View Modes</li>
+                        <li className={cls.strategy_text__list__item}>Your History, Saved</li>
+                    </ul>
+                    <div className={cls.m_title_container__btn_container}>
+                        <button>
+                            Try for free
+                        </button>
+                    </div>
+                </div>
+                <div className={cls.strategy_img}>
+                    <img src={screenShot} alt={'Screenshot'}/>
+                </div>
+            </div>
+
             <div className={cls.skills_container}>
                 <div className={cls.skills_title}>
                     Fuel your skills, <span>multiply your wins</span>
@@ -110,6 +138,69 @@ export const MainSec = ({ cls }) => {
                 </div>
                 <div className={cls.skills_btn}>
                     Compare Postflopizer with other GTO solvers >
+                </div>
+            </div>
+
+            <div className={cls.price_container}>
+                <div className={cls.skills_title}>
+                    Fair prices for fair play
+                </div>
+                <div>
+                    <div className={cls.price_container_flex}>
+                        <div className={cls.price_item}>
+                            <div className={cls.price_item__header}></div>
+                            <div className={cls.price_item__title}>
+                                Free
+                            </div>
+                            <ul className={cls.price_item__list}>
+                                <li className={cls.price_item__list__item}>Great way to explore the features</li>
+                                <li className={cls.price_item__list__item}>All tools except flop editing</li>
+                            </ul>
+                            <div className={cls.price_item__price}>
+                                $0
+                            </div>
+                            <div className={cls.price_item__container_btn}>
+                                <button className={cls.price_item__btn} >
+                                    Try it for free
+                                </button>
+                            </div>
+                        </div>
+                        <div className={cls.price_item__icm}>
+                            <div className={cls.price_item__icm__header}></div>
+                            <div className={cls.price_item__icm__title}>
+                                Standart
+                            </div>
+                            <ul className={cls.price_item__icm__list}>
+                                <li className={cls.price_item__icm__list__item}>Full version</li>
+                                <li className={cls.price_item__icm__list__item}>All features included</li>
+                                <li className={cls.price_item__icm__list__item}>More features yet to come</li>
+                            </ul>
+                            <div className={cls.price_item__icm__price}>
+                                <span className={cls.price_item__icm__price__true}>$300</span>
+                                <span className={cls.price_item__icm__price__sale}>$270</span>
+                                <div className={cls.price_item__icm__price__time}>
+                                    One-time purchase
+                                </div>
+                            </div>
+                            <div className={cls.price_item__icm__container_btn}>
+                                <button className={cls.price_item__icm__btn} >
+                                    Buy the Standard Pack
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={cls.price_options_container}>
+                <div className={cls.price_options__text}>
+                    <div className={cls.price_options__text__title}>
+                        “All I wanted to do was create a postflop GTO solver that wouldn't induce nosebleeds. I believe we've succeeded.”
+                    </div>
+                    <div className={cls.price_options__text__subtitle}>Valentin Kuzub, CEO and Founder</div>
+                </div>
+                <div className={cls.price_options__btn_container}>
+                    <button className={cls.price_options__btn_container__btn}>Pricing options</button>
                 </div>
             </div>
         </div>
